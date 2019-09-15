a51e7d5918dc436803e60a9f0a66970102fc764e
Ranganath <ranganathmd1995@gmail.com>
2019-09-15 09:47:16 +0530

    routes and links separated

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Link, BrowserRouter, Route, Switch } from "react-router-dom"
-
+import { Layout } from 'antd';
+import { BrowserRouter } from "react-router-dom"
 import AppBar from "./AppBar"
-import Footer from "./Footer"
+import AppFooter from "./AppFooter"
 import Routes from "./routes"
 
-class App extends React.Component{
-  render(){
+const { Header, Footer, Sider, Content } = Layout;
+
+class App extends React.Component {
+
+  render() {
     return (
       <BrowserRouter>
-        <div>
-          <AppBar />
-          <Link to="/">Home</Link>
-          <Link to="/login">Sign In</Link>
-          <Link to="/register">Sign up</Link>
-          <Link to="/logout">Log out</Link>
-          <Link to="/myaccount">My Account</Link>
-        </div>
-        <Routes />
-        <Footer />
+        <Layout>
+          <Header>
+            <AppBar />
+          </Header>
+          <Content>
+            <Routes />
+          </Content>
+          <Footer>
+            <AppFooter />
+          </Footer>
+        </Layout>
       </BrowserRouter>
     );
   }
--- client/src/AppBar.js
+++ client/src/AppBar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 export default class AppBar extends Component {
     render() {
         return (
             <div>
-                <h2>App bar Comes here</h2>
+                <Link to="/">Home</Link>
+                <Link to="/login">Sign In</Link>
+                <Link to="/register">Sign up</Link>
+                <Link to="/logout">Log out</Link>
+                <Link to="/myaccount">My Account</Link>
             </div>
         )
     }
